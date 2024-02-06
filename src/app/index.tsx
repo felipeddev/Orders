236a04883e48908f955f29a248dcf0bea1e8fb2a
Felipe D. Silva <feliped.dev@proton.me>
2024-02-06 02:57:03 -0300

    Initial commit
    Generated by create-expo-app 2.1.1.

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="bg-slate-900 flex-1">
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
