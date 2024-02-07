d7d2aa0f8d32e8cfa2546a2232679c53a7874677
Felipe D. Silva <feliped.dev@proton.me>
2024-02-06 23:10:55 -0300

    feat: create interface

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,37 @@
-import { Text, View } from "react-native";
+import {useState} from 'react';
+import {FlatList, View} from 'react-native';
 
-export default function App() {
-  return (
-    <View className="bg-slate-900 flex-1">
-      <Text>Hello World!</Text>
-    </View>
-  );
+import {CATEGORIES} from '@/utils/data/products';
+
+import {Header} from '@/components/Header';
+import {CategoryButton} from '@/components/CategoryButton';
+
+export default function Home() {
+    const [category, setCategory] = useState(CATEGORIES[0]);
+
+    function handleCategorySelect(selectedCategory: string) {
+        setCategory(selectedCategory);
+    }
+
+    return (
+        <View className="flex-1 pt-8">
+            <Header title="Faça seu pedido" cartQuantityItems={3} />
+
+            <FlatList
+                data={CATEGORIES}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => (
+                    <CategoryButton
+                        title={item}
+                        isSelected={item === category}
+                        onPress={() => handleCategorySelect(item)}
+                    />
+                )}
+                horizontal
+                className="max-h-10 mt-5"
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+            />
+        </View>
+    );
 }
